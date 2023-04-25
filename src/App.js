a455a34adee10fa2c8c31f14ac193420e935964e
Sodara Sou <sodarasou168@gmail.com>
2023-04-25 14:15:57 +0700

    Add Header and Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <>
-      <p>Hello World</p>
-      <p>I like Pizza!</p>
+      <Header />
+      <div className="container">
+        <Navbar />
+      </div>
     </>
   );
 }
-
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+function Header() {
+  const cubeTextStyles = {
+    color: "rgb(255, 69, 0)",
+  };
+
+  return (
+    <header>
+      <h1>
+        <span style={cubeTextStyles}>CUBE</span> GAMING STORE
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
